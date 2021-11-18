8964fe027b7d81962b661fd18690571866bb64b9
Victoria-Sampalo <vikytasampalo@gmail.com>
2021-11-18 06:53:06 +0100

    Subida inicial desde USB debido a fallo en clase de creación.

---
+++ src/main/java/contrasenya/Contrasenya.java
@@ -0,0 +1,34 @@
+/*
+ *
+ */
+package contrasenya;
+
+import java.util.Random;
+
+public class Contrasenya {
+
+    public static void main(String[] args) {
+     //--------VARIABLES ----------
+     int num1, num2, num3, num4;
+     
+     //OBJETO CLASE DE TIPO RANDOM.
+     
+     Random numAleatorio = new Random(); 
+     do{
+     for(int i=0; i<5; i++){
+         System.out.println(numAleatorio.nextInt(10));
+     
+     }
+     
+     }while ();
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
