0e8b36ad4ee61ea22f91a086406e6b9c2b107bcb
Victoria Sampalo Garcia <vikytasampalo@gmail.com>
2022-01-11 14:41:57 +0100

    ejercicio 9. falta instanciación

---
+++ src/main/java/relacionc/Nif.java
@@ -0,0 +1,158 @@
+/*
+ * EJERCICIO 9
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+package relacionc;
+
+/**
+ *
+ * @author victoria
+ */
+public class Nif {
+    //DEFINO ATRIBUTOS CON LOS QUE VOY A TRABAJAR
+    public String numeroDni;
+    private char letraDni;
+
+    public Nif(String numeroDni) {
+        this.numeroDni = numeroDni;
+        this.letraDni= CrearLetraDni();
+        
+    }
+    
+    private char CrearLetraDni () {
+        
+        char letra = ' ';
+        //revisar longitud del string
+        int dnilongitud = numeroDni.length();
+        
+        //restricción si el largo es 8 continua.
+        if (dnilongitud==8){
+         int numeroInt = Integer.parseInt(numeroDni);
+         //la letra es la división de
+         int letraDni = numeroInt % 23;
+         
+        switch (letraDni){
+            case 0:
+                letra = 'T';
+                break;
+            case 1:
+                letra = 'R';
+                break;
+            case 2:
+                letra = 'w';
+                break;
+            case 3:
+                letra = 'A';
+                break;
+            case 4:
+                letra = 'G';
+                break;
+            case 5:
+                letra = 'M';
+                break;
+            case 6: 
+                letra = 'Y';
+                break;
+            case 7:
+                letra = 'F';
+                break; 
+            case 8:
+                letra = 'P';
+                break;
+            case 9:
+                letra = 'D';
+                break;
+            
+            case 10:
+                letra = 'X';
+                break;
+            
+            case 11:
+                letra = 'B';
+                break;
+            case 12:
+                letra = 'N';
+                break;
+            case 13:
+                letra = 'J';
+                break; 
+            case 14:
+                letra = 'Z';
+                break;
+            case 15:
+                letra = 'S';
+                break;
+            
+            case 16:
+                letra = 'Q';
+                break;
+            
+            case 17:
+                letra = 'V';
+                break;
+            case 18:
+                letra = 'H';
+                break;
+               
+            case 19:
+                letra = 'L';
+                break;
+            
+            case 20:
+                letra = 'C';
+                break;
+            
+            case 21:
+                letra = 'K';
+                break;
+            case 22:
+                letra = 'E';
+                break;
+        
+        }
+        }
+        else {
+         this.numeroDni = "0";
+        }
+        
+        
+       return letra;
+    
+    }
+
+    public String getNumeroDni() {
+        return numeroDni;
+    }
+
+    public char getLetraDni() {
+        return letraDni;
+    }
+    
+    
+    
+    
+
+    @Override
+    public String toString() {
+        return "Nif{" + numeroDni + "-" + letraDni + '}';
+    }
+    
+    
+    public static void main(String[] args) {
+        //creo el objeto
+        Nif nif1 = new Nif("79055940");
+        System.out.println(nif1.toString());
+    }
+    
+    
+           
+
+    
+    
+    
+}
---
+++ src/main/java/relacionc/TestNif.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package relacionc;
+
+/**
+ *
+ * @author victoria
+ */
+public class TestNif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //creo objeto
+        Nif nif1 = new Nif("7955940");
+        
+    }
+    
+}
